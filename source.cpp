975ba700ef56fb0bd0bdbf3f93cd14f27c8a37d7
MustafaGood <mustafa.swedish@gmail.com>
2023-03-09 03:17:10 -0800

    Create a (regular) public method Print in Element that prints the element

--- source.cpp
+++ source.cpp
@@ -16,6 +16,12 @@ public:
         this->melting_point = melting_point;
         this->boiling_point = boiling_point;
     }
+     void Print() {
+        std::cout << "Element: " << name << std::endl;
+        std::cout << "Type: " << type << std::endl;
+        std::cout << "Melting Point: " << melting_point << " K" << std::endl;
+        std::cout << "Boiling Point: " << boiling_point << " K" << std::endl;
+    }
 
 };
 
@@ -23,5 +29,9 @@ int main() {
     Basic oxygen("Oxygen", 8, "Non-metal", 54.36, 90.188);
     Basic iron("Iron", 26, "Metal", 1811, 3134);
     Basic gold("Gold", 79, "Metal", 1337.33, 3243);
+    oxygen.Print();
+    iron.Print();
+    gold.Print();
+
     return 0;
 }
